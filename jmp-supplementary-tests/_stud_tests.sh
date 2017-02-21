533c0f0ee462c8aea2ea820fc0022d2a2307b472
Matla <matla@matla.cz>
2017-02-21 08:48:00 +0100

    Uprava školního skripru přidání porovnání a výpisů

--- jmp-supplementary-tests/_stud_tests.sh
+++ jmp-supplementary-tests/_stud_tests.sh
@@ -52,8 +52,31 @@
 #
 # =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
-TASK=jmp
-INTERPRETER="php5.6"
+#---------------------------------------------------------------------------------------#
+#                                   Moje                                                #
+#---------------------------------------------------------------------------------------#
+
+function format {
+	if [ $1 != 0 ]; then
+               echo -e "\e[31mX\e[0m"
+            else
+               echo -e "\e[32mOK\e[0m"
+            fi
+} 
+
+function comper {  
+	`diff -N "./$1.out" "./ref-out/$1.out" > /dev/null`;
+	out=$(format $?)
+	`diff -N "./$1.err" "./ref-out/$1.err" > /dev/null`;
+	err=$(format $?)
+	`diff -N "./$1.!!!" "./ref-out/$1.!!!" > /dev/null`;
+	cod=$(format $?)
+	echo -e "$1 [out=$out err=$err cod=$cod]\n";
+}
+#########################################################################################
+
+TASK="../FIT_IPP_1_MakroProcesor/jmp"
+INTERPRETER="php"
 EXTENSION=php
 #INTERPRETER="python3.6"
 #EXTENSION=py
@@ -69,79 +92,98 @@ LOCAL_OUT_PATH3="`pwd`/" #Alternative 2 (absolute path)
 LOG_PATH="./"
 
 
-# test01: Zpracovani vstupu I; Expected output: test01.out; Expected return code: 0
+echo "#test01: Zpracovani vstupu I; Expected output: test01.out; Expected return code: 0"
 $INTERPRETER $TASK.$EXTENSION --input=${LOCAL_IN_PATH2}test01.in --output=${LOCAL_OUT_PATH2}test01.out 2> ${LOG_PATH}test01.err
 echo -n $? > test01.!!!
+comper "test01"
 
-# test02: Zpracovani vstupu II; Expected output: test02.out; Expected return code: 0
+echo "# test02: Zpracovani vstupu II; Expected output: test02.out; Expected return code: 0"
 $INTERPRETER $TASK.$EXTENSION --input=${LOCAL_IN_PATH2}test02.in --output="${LOCAL_OUT_PATH3}test02.out" 2> ${LOG_PATH}test02.err
 echo -n $? > test02.!!!
+comper "test02"
 
-# test03: Zpracovani vstupu III; Expected output: test03.out; Expected return code: 0
+echo "# test03: Zpracovani vstupu III; Expected output: test03.out; Expected return code: 0"
 $INTERPRETER $TASK.$EXTENSION --input=${LOCAL_IN_PATH}test03.in --output="${LOCAL_OUT_PATH3}test03.out" 2> ${LOG_PATH}test03.err
 echo -n $? > test03.!!!
+comper "test03"
 
-# test04: Zpracovani vstupu IV; Expected output: test04.out; Expected return code: 0
+echo "# test04: Zpracovani vstupu IV; Expected output: test04.out; Expected return code: 0"
 $INTERPRETER $TASK.$EXTENSION --input="${LOCAL_IN_PATH3}test04.in" --output=${LOCAL_OUT_PATH2}test04.out 2> ${LOG_PATH}test04.err
 echo -n $? > test04.!!!
+comper "test04"
 
-# test05: Chybny vstup I; Expected output: test05.out; Expected return code: 55
+echo "# test05: Chybny vstup I; Expected output: test05.out; Expected return code: 55"
 $INTERPRETER $TASK.$EXTENSION --input="${LOCAL_IN_PATH3}test05.in" --output=${LOCAL_OUT_PATH}test05.out 2> ${LOG_PATH}test05.err
 echo -n $? > test05.!!!
+comper "test05"
 
-# test06: Chybny vstup II; Expected output: test06.out; Expected return code: 55
+echo "# test06: Chybny vstup II; Expected output: test06.out; Expected return code: 55"
 $INTERPRETER $TASK.$EXTENSION --input="${LOCAL_IN_PATH3}test06.in" --output=${LOCAL_OUT_PATH}test06.out 2> ${LOG_PATH}test06.err
 echo -n $? > test06.!!!
+comper "test06"
 
-# test07: Chybny vstup III; Expected output: test07.out; Expected return code: 55
+echo "# test07: Chybny vstup III; Expected output: test07.out; Expected return code: 55"
 $INTERPRETER $TASK.$EXTENSION --input=${LOCAL_IN_PATH}test07.in --output="${LOCAL_OUT_PATH3}test07.out" 2> ${LOG_PATH}test07.err
 echo -n $? > test07.!!!
+comper "test07"
 
-# test08: Chybny vstup IV; Expected output: test08.out; Expected return code: 55
+echo "# test08: Chybny vstup IV; Expected output: test08.out; Expected return code: 55"
 $INTERPRETER $TASK.$EXTENSION --input=${LOCAL_IN_PATH2}test08.in --output="${LOCAL_OUT_PATH3}test08.out" 2> ${LOG_PATH}test08.err
 echo -n $? > test08.!!!
+comper "test08"
 
-# test09: Jednoduche makro bez parametru; Expected output: test09.out; Expected return code: 0
+echo "# test09: Jednoduche makro bez parametru; Expected output: test09.out; Expected return code: 0"
 $INTERPRETER $TASK.$EXTENSION --input=${LOCAL_IN_PATH}test09.in --output=${LOCAL_OUT_PATH}test09.out 2> ${LOG_PATH}test09.err
 echo -n $? > test09.!!!
+comper "test09"
 
-# test10: Jednoduche makro s jednim parametrem; Expected output: test10.out; Expected return code: 0
+echo "# test10: Jednoduche makro s jednim parametrem; Expected output: test10.out; Expected return code: 0"
 $INTERPRETER $TASK.$EXTENSION --input=${LOCAL_IN_PATH}test10.in --output="${LOCAL_OUT_PATH3}test10.out" 2> ${LOG_PATH}test10.err
 echo -n $? > test10.!!!
+comper "test10"
 
-# test11: Jednoduche makro s vice parametry; Expected output: test11.out; Expected return code: 0
+echo "# test11: Jednoduche makro s vice parametry; Expected output: test11.out; Expected return code: 0"
 $INTERPRETER $TASK.$EXTENSION --input=${LOCAL_IN_PATH2}test11.in --output="${LOCAL_OUT_PATH3}test11.out" 2> ${LOG_PATH}test11.err
 echo -n $? > test11.!!!
+comper "test11"
 
-# test12: Chyba v parametrech; Expected output: test12.out; Expected return code: 55
+echo "# test12: Chyba v parametrech; Expected output: test12.out; Expected return code: 55"
 $INTERPRETER $TASK.$EXTENSION --input=${LOCAL_IN_PATH}test12.in --output="${LOCAL_OUT_PATH3}test12.out" 2> ${LOG_PATH}test12.err
 echo -n $? > test12.!!!
+comper "test12"
 
-# test13: Vstup z parametru; Expected output: test13.out; Expected return code: 0
+echo "# test13: Vstup z parametru; Expected output: test13.out; Expected return code: 0"
 $INTERPRETER $TASK.$EXTENSION "--cmd={{*}}@def@x{}{ ahoj}" --input=${LOCAL_IN_PATH2}test13.in --output="${LOCAL_OUT_PATH3}test13.out" 2> ${LOG_PATH}test13.err
 echo -n $? > test13.!!!
+comper "test13"
 
-# test14: Nedefinovane makro; Expected output: test14.out; Expected return code: 56
+echo "# test14: Nedefinovane makro; Expected output: test14.out; Expected return code: 56"
 $INTERPRETER $TASK.$EXTENSION --input=${LOCAL_IN_PATH}test14.in --output=${LOCAL_OUT_PATH2}test14.out 2> ${LOG_PATH}test14.err
 echo -n $? > test14.!!!
+comper "test14"
 
-# test15: Volny parametr v tele makra; Expected output: test15.out; Expected return code: 55
+echo "# test15: Volny parametr v tele makra; Expected output: test15.out; Expected return code: 55"
 $INTERPRETER $TASK.$EXTENSION --input=${LOCAL_IN_PATH}test15.in --output=${LOCAL_OUT_PATH}test15.out 2> ${LOG_PATH}test15.err
 echo -n $? > test15.!!!
+comper "test15"
 
-# test16: Makro bez efektu; Expected output: test16.out; Expected return code: 0
+echo "# test16: Makro bez efektu; Expected output: test16.out; Expected return code: 0"
 $INTERPRETER $TASK.$EXTENSION --input="${LOCAL_IN_PATH3}test16.in" --output=${LOCAL_OUT_PATH}test16.out 2> ${LOG_PATH}test16.err
 echo -n $? > test16.!!!
+comper "test16"
 
-# test17: Zruseni definice makra; Expected output: test17.out; Expected return code: 56
+echo "# test17: Zruseni definice makra; Expected output: test17.out; Expected return code: 56"
 $INTERPRETER $TASK.$EXTENSION --input=${LOCAL_IN_PATH}test17.in --output="${LOCAL_OUT_PATH3}test17.out" 2> ${LOG_PATH}test17.err
 echo -n $? > test17.!!!
+comper "test17"
 
-# test18: Pokus o vymazani chraneneho makra; Expected output: test18.out; Expected return code: 57
+echo "# test18: Pokus o vymazani chraneneho makra; Expected output: test18.out; Expected return code: 57"
 $INTERPRETER $TASK.$EXTENSION --input=${LOCAL_IN_PATH}test18.in --output="${LOCAL_OUT_PATH3}test18.out" 2> ${LOG_PATH}test18.err
 echo -n $? > test18.!!!
+comper "test18"
 
-# test19: Komplexni test @undef & @set; Expected output: test19.out; Expected return code: 0
+echo "# test19: Komplexni test @undef & @set; Expected output: test19.out; Expected return code: 0"
 $INTERPRETER $TASK.$EXTENSION --input=${LOCAL_IN_PATH}test19.in --output="${LOCAL_OUT_PATH3}test19.out" 2> ${LOG_PATH}test19.err
 echo -n $? > test19.!!!
+comper "test19"
 
